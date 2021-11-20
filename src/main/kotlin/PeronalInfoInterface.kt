a03aa00ce714e39d656a81407420767bc5b7ee61
mubeen riaz <mubeen.aplome@gmail.com>
2021-11-20 12:16:43 +0500

    type checking, type casting, smart type casting

---
+++ src/main/kotlin/PeronalInfoInterface.kt
@@ -0,0 +1,47 @@
+interface PeronalInfoInterface {
+
+    val providerInfo : String
+
+    fun printInfo(person: Person){
+        print(providerInfo)
+        print(person.printInfo())
+    }
+}
+
+interface SessionInfoInterface{
+    fun getSessionId(): String
+}
+
+class BasicInfoProvider : PeronalInfoInterface, SessionInfoInterface{
+    override val providerInfo: String
+        get() = "this is the interface property"
+
+    override fun printInfo(person: Person) {
+        super.printInfo(person)
+        print("some additional text")
+    }
+
+    override fun getSessionId(): String {
+        return "this is Session id"
+    }
+}
+
+fun main() {
+    val info = BasicInfoProvider()
+    //info.printInfo(Person())
+    checkTypes(info)
+}
+
+fun checkTypes(infoProvider: PeronalInfoInterface){
+    //type checking
+    if(infoProvider !is SessionInfoInterface){
+        print("isn't a session info provider")
+    }else {
+        println("is a session info provider")
+        //type casting
+        println((infoProvider as SessionInfoInterface).getSessionId())
+        //smart casting
+        println(infoProvider.getSessionId())
+    }
+}
+
